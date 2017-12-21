4997ca8b64dc34d0ae57be8b86ace88618b5293b
Po <po.grammer.lin@gmail.com>
2017-12-21 13:36:36 -0600

    Completed Java III Sessions and Cookies Exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,21 +3,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null) {
+            if(session.getAttribute("user").equals("admin")) {
+                response.sendRedirect("/profile");
+                return;
+            }
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
             response.sendRedirect("/profile");
+            HttpSession session = request.getSession();
+            session.setAttribute("user",username);
         } else {
             response.sendRedirect("/login");
         }
